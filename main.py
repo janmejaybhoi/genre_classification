0a221c4ee76be8bf76fa7195558c3ce3c36a9316
root <root@DESKTOP-FPUKNA9.localdomain>
2022-03-27 16:23:25 +0530

    Some minor changes

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ def go(config: DictConfig):
             os.path.join(root_path, "evaluate"),
             "main",
             parameters={
-                "model_export":config['random_forest_pipeline']['export_artifact'],
+                "model_export": f"{config['random_forest_pipeline']['export_artifact']}:latest",
                 "test_data": "data_test.csv:latest"
 
             },
